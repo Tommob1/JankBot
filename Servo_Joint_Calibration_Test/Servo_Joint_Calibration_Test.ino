b79d2f25816197a2d102420716d64908de2d7071
Brayden Tomlinson <humortronic@gmail.com>
2024-05-21 14:46:51 +0930

    Added changes to code

--- Servo_Joint_Calibration_Test/Servo_Joint_Calibration_Test.ino
+++ Servo_Joint_Calibration_Test/Servo_Joint_Calibration_Test.ino
@@ -9,27 +9,23 @@ void setup() {
 }
 
 void loop() {
-  // Move servo1 from 0 to 180 degrees
   for (int pos1 = 0; pos1 <= 180; pos1 += 1) {
     servo1.write(pos1);
-    delay(15); // Delay for servo1 can be different if desired
+    delay(15);
   }
 
-  // Move servo2 from 0 to 180 degrees, independently
   for (int pos2 = 0; pos2 <= 180; pos2 += 1) {
     servo2.write(pos2);
-    delay(5); // Delay for servo2 can be different if desired
+    delay(5);
   }
 
-  // Move servo1 back from 180 to 0 degrees
   for (int pos1 = 180; pos1 >= 0; pos1 -= 1) {
     servo1.write(pos1);
-    delay(15); // Delay for servo1 can be different if desired
+    delay(15);
   }
 
-  // Move servo2 back from 180 to 0 degrees, independently
   for (int pos2 = 180; pos2 >= 0; pos2 -= 1) {
     servo2.write(pos2);
-    delay(5); // Delay for servo2 can be different if desired
+    delay(5);
   }
 }
