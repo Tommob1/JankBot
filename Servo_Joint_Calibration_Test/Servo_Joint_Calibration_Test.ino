613b78e622fa482e393d3e7558d8cbea12235bb0
Brayden Tomlinson <humortronic@gmail.com>
2024-05-27 21:11:32 +0930

    Added changes to code

--- Servo_Joint_Calibration_Test/Servo_Joint_Calibration_Test.ino
+++ Servo_Joint_Calibration_Test/Servo_Joint_Calibration_Test.ino
@@ -2,30 +2,68 @@
 
 Servo servo1;
 Servo servo2;
+Servo servo3;
+
+int servo1Pos;
+int servo2Pos;
+int servo3Pos;
+
+int servo1Left = 10;
+int servo1Right = 170;
+int servo2Up = 10;
+int servo2Down = 170;
+int servo3Min = 10;
+int servo3Max = 170;
 
 void setup() {
-  servo1.attach(10);
-  servo2.attach(11);
+  servo1.attach(11);
+  servo2.attach(10);
+  servo3.attach(9);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (int pos1 = 0; pos1 <= 180; pos1 += 1) {
-    servo1.write(pos1);
+  // Move servo1 to the left
+  for (servo1Pos = servo1Right; servo1Pos >= servo1Left; servo1Pos -= 1) {
+    servo1.write(servo1Pos);
     delay(15);
   }
 
-  for (int pos2 = 0; pos2 <= 180; pos2 += 1) {
-    servo2.write(pos2);
-    delay(5);
-  }
-
-  for (int pos1 = 180; pos1 >= 0; pos1 -= 1) {
-    servo1.write(pos1);
+  // Move servo2 down (servo3 moves in the opposite direction)
+  for (servo2Pos = servo2Up; servo2Pos <= servo2Down; servo2Pos += 1) {
+    servo2.write(servo2Pos);
+    servo3Pos = map(servo2Pos, servo2Up, servo2Down, servo3Min, servo3Max); // Opposite direction
+    servo3.write(servo3Pos);
     delay(15);
   }
 
-  for (int pos2 = 180; pos2 >= 0; pos2 -= 1) {
-    servo2.write(pos2);
-    delay(5);
+  // Move servo2 up (servo3 moves in the opposite direction)
+  for (servo2Pos = servo2Down; servo2Pos >= servo2Up; servo2Pos -= 1) {
+    servo2.write(servo2Pos);
+    servo3Pos = map(servo2Pos, servo2Down, servo2Up, servo3Max, servo3Min); // Opposite direction
+    servo3.write(servo3Pos);
+    delay(15);
+  }
+
+  // Move servo1 to the right
+  for (servo1Pos = servo1Left; servo1Pos <= servo1Right; servo1Pos += 1) {
+    servo1.write(servo1Pos);
+    delay(15);
+  }
+
+  // Move servo2 down (servo3 moves in the opposite direction)
+  for (servo2Pos = servo2Up; servo2Pos <= servo2Down; servo2Pos += 1) {
+    servo2.write(servo2Pos);
+    servo3Pos = map(servo2Pos, servo2Up, servo2Down, servo3Min, servo3Max); // Opposite direction
+    servo3.write(servo3Pos);
+    delay(15);
+  }
+
+  // Move servo2 up (servo3 moves in the opposite direction)
+  for (servo2Pos = servo2Down; servo2Pos >= servo2Up; servo2Pos -= 1) {
+    servo2.write(servo2Pos);
+    servo3Pos = map(servo2Pos, servo2Down, servo2Up, servo3Max, servo3Min); // Opposite direction
+    servo3.write(servo3Pos);
+    delay(15);
   }
 }
