59fa475803ff08ba830bec6024530614702cbf58
Brayden Tomlinson <humortronic@gmail.com>
2024-07-01 14:29:10 +0930

    Added changes to code

--- Servo_Joint_Calibration_Test/Servo_Joint_Calibration_Test.ino
+++ Servo_Joint_Calibration_Test/Servo_Joint_Calibration_Test.ino
@@ -32,14 +32,14 @@ void loop() {
     servo2.write(servo2Pos);
     servo3Pos = map(servo2Pos, servo2Up, servo2Down, servo3Min, servo3Max);
     servo3.write(servo3Pos);
-    delay(10);
+    delay(5);
   }
 
   for (servo2Pos = servo2Down; servo2Pos >= servo2Up; servo2Pos -= 1) {
     servo2.write(servo2Pos);
     servo3Pos = map(servo2Pos, servo2Down, servo2Up, servo3Max, servo3Min);
     servo3.write(servo3Pos);
-    delay(10);
+    delay(5);
   }
 
   for (servo1Pos = servo1Left; servo1Pos <= servo1Right; servo1Pos += 1) {
@@ -51,13 +51,13 @@ void loop() {
     servo2.write(servo2Pos);
     servo3Pos = map(servo2Pos, servo2Up, servo2Down, servo3Min, servo3Max);
     servo3.write(servo3Pos);
-    delay(10);
+    delay(5);
   }
 
   for (servo2Pos = servo2Down; servo2Pos >= servo2Up; servo2Pos -= 1) {
     servo2.write(servo2Pos);
     servo3Pos = map(servo2Pos, servo2Down, servo2Up, servo3Max, servo3Min);
     servo3.write(servo3Pos);
-    delay(10);
+    delay(5);
   }
 }
