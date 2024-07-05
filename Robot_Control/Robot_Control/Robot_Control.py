7b4495417f62bda992e94b799a85233044654283
Brayden Tomlinson <humortronic@gmail.com>
2024-07-05 12:19:23 +0930

    Added changes to code

--- Robot_Control/Robot_Control/Robot_Control.py
+++ Robot_Control/Robot_Control/Robot_Control.py
@@ -3,7 +3,7 @@ from pynput import mouse
 import tkinter as tk
 import struct
 
-ser = serial.Serial('/dev/tty.usbmodem11401', 9600)  # Update this to your specific port
+ser = serial.Serial('/dev/tty.usbmodem11401', 9600)
 mouse_x, mouse_y = 0, 0
 servo1_pos, servo2_pos, servo3_pos = 90, 90, 90
 listener = None
@@ -11,8 +11,8 @@ listener = None
 def on_move(x, y):
     global mouse_x, mouse_y, servo1_pos, servo2_pos, servo3_pos
     mouse_x, mouse_y = x, y
-    servo1_pos = int(map_value(mouse_x, 0, 1920, 10, 170))  # Adjust screen height as needed
-    servo2_pos = int(map_value(mouse_y, 0, 1080, 10, 170))  # Adjust screen width as needed
+    servo1_pos = int(map_value(mouse_x, 0, 1920, 10, 170))
+    servo2_pos = int(map_value(mouse_y, 0, 1080, 10, 170))
     servo3_pos = int(map_value(servo2_pos, 10, 170, 10, 170))
     update_telemetry()
     send_command()
