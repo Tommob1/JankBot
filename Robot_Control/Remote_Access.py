de307f5edf8cb27476102a612c646202e6682a79
Brayden Tomlinson <humortronic@gmail.com>
2025-02-28 16:14:08 +1030

    Added changes to code

--- Robot_Control/Remote_Access.py
+++ Robot_Control/Remote_Access.py
@@ -1,3 +1,11 @@
 import sys
 import serial
 import pynput
+
+def find_arduino_port():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if 'Arduino' in port.description or 'usbmodem' in port.device:
+            return port.device
+    return None
+
